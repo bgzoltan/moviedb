128e012dbb7e47c91c4228552329a6a4470cb6b0
bgzoltan <bgzoltan@gmail.com>
2022-07-31 09:44:05 +0200

    Tailwind has added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO MOVIEDB</div>;
+  return (
+    <div className="grid place-items-center text-3xl font-bold h-screen text-green-600">
+      <h1>Hello MovieDB!</h1>
+    </div>
+  );
 }
 
 export default App;
