79f4b713070e9531194bdbfce93b919253a74f9e
bgzoltan <bgzoltan@gmail.com>
2022-07-31 09:57:43 +0200

    MoviesList component has created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./App.css";
+import MoviesList from "./components/MoviesList";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className="grid place-items-center text-3xl font-bold h-screen text-green-600">
-      <h1>Hello MovieDB!</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-col items-center font-bold h-screen box-border p-0 m-0">
+        <h1 className="text-5xl text-white bg-gray-400 w-full text-center">
+          Hello MovieDB!
+        </h1>
+        <MoviesList />
+      </div>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/MoviesList.tsx
@@ -0,0 +1,9 @@
+const MoviesList = () => {
+  return (
+    <div>
+      <h2 className="text-2xl text-gray-400">List of movies</h2>
+    </div>
+  );
+};
+
+export default MoviesList;
